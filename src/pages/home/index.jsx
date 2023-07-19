66fb521c382ebb5bf22f3532c40ac3f69154537a
Nico Ramirez <ramireznicc.23@gmail.com>
2023-07-19 15:52:04 +0200

    changing app title

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -12,15 +12,21 @@ function Home() {
     setDisplay(false);
   }, 5000);
 
+  const handleSetDisplay = () => {
+    setDisplay(!display);
+  };
+
   return (
     <div className="container home">
       <div className="home-container">
         <h3 className={display ? "show" : "hidden"}>Welcome to</h3>
-        <h1>The typing game</h1>
+        <h1>the Typing game</h1>
         <h4>Let's see how fast you can type.</h4>
       </div>
       <Link to="/game">
-        <Button icon="faPlay">Start</Button>
+        <Button icon="faPlay" onClick={handleSetDisplay}>
+          Start
+        </Button>
       </Link>
     </div>
   );
